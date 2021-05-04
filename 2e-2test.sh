a56118f5ee3c994c2b19650203741517c482d67e
meso272 <liujinyang1201@126.com>
2021-05-04 03:10:39 -0700

    dd

--- 2e-2test.sh
+++ 2e-2test.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 source ~/.bashrc
 conda activate tf
+cd lossycompression/autoencoder
 python do_aramco.py aramco 0 2e-2res/aramco 2
 python do_Hurricane.py U 0 2e-2res/U 2
 python do_Hurricane.py QVAPOR 0 2e-2res/QVAPOR 2
