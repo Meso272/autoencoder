36b71303f8a69fe6e6e40793812df021ede31c24
meso272 <liujinyang1201@126.com>
2021-05-04 03:08:46 -0700

    dd

---
+++ 2e-2test.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+source ~/.bashrc
+conda activate tf
+python do_aramco.py aramco 0 2e-2res/aramco 2
+python do_Hurricane.py U 0 2e-2res/U 2
+python do_Hurricane.py QVAPOR 0 2e-2res/QVAPOR 2
+python do_Nyx.py baryon_density 0 2e-2res/bdlog 2
+python do_Nyx.py temperature 0 2e-2res/tlog 2
+python do_Nyx.py dark_matter_density 0 2e-2res/dmdlog 2
